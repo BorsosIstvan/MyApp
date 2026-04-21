f0f9121834bc75893acd608dbe6c8d2a1f12dea9
BorsosIstvan <bistvanus@gmail.com>
2026-04-21 22:30:53 +0200

    myapp nieuw

--- details.php
+++ details.php
@@ -15,10 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save_details'])) {
     $db->exec("ALTER TABLE clients ADD COLUMN phone TEXT");
 
     $update = $db->prepare("UPDATE clients SET address = ?, phone = ? WHERE id = ?");
-    //$update->execute([$address, $phone, $id]);
-	if (!$update->execute([$address, $phone, $id])) {
-    print_r($update->errorInfo());
-    exit;
+    $update->execute([$address, $phone, $id]);
 }
     
     // Foto uploaden
